205e1f1198a66379ba07227c36bee660e0c925c4
Fabio <fabiopagoti@gmail.com>
2017-09-21 20:56:51 -0300

    parte 1 ok

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+	res
+		.status(200)
+		.send('Hello World')
+})
+
+app.listen(3000, (err) => {
+	console.log('Is up')
+})
