1d970ed7dc4a4a5d820deb4bbf260c7ac2518757
Fabio <fabiopagoti@gmail.com>
2017-09-21 21:05:04 -0300

    mongoose criado com schema de cliente

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/emprestimos')
+
+const ClienteSchema = {
+	nome: String,
+	renda: Number,
+	email: String,
+	senha: String,
+	emprestimos: Array
+}
+
+const Cliente = mongoose.model('Cliente', ClienteSchema);
 
 app.get('/hello', (req, res) => {
 	res
