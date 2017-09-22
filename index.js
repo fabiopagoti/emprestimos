073a0caa3599b3449eb1c3eb7307cce91489423f
Fabio <fabiopagoti@gmail.com>
2017-09-21 21:13:55 -0300

    body parser adicionado

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/emprestimos')
 
 const ClienteSchema = {
-	nome: String,
-	renda: Number,
-	email: String,
-	senha: String,
-	emprestimos: Array
+	nome: {
+		type: String,
+		required: true
+	},
+	renda: {
+		type: Number,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	senha: {
+		type: String,
+		required: true
+	},
+	emprestimos: {
+		type: Array,
+		required: false
+	}
 }
 
 const Cliente = mongoose.model('Cliente', ClienteSchema);
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => {
 	res
-		.status(200)
-		.send('Hello World')
+	.status(200)
+	.send('Hello World')
+})
+
+app.post('/cliente', (req, res) => {
+
 })
 
 app.listen(3000, (err) => {
